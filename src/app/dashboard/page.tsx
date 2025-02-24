8fe6aa01304f69c4878076756863d25237ddf038
Zac Jones <zacjones93@gmail.com>
2025-02-24 09:28:48 -0700

    design: update official score cta to card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,12 +12,20 @@ import {
 	LogWorkoutFormSkeleton,
 	AssignPointsFormSkeleton,
 } from "./_components/loading-skeletons";
+import {
+	Card,
+	CardHeader,
+	CardTitle,
+	CardDescription,
+	CardContent,
+	CardFooter,
+} from "@/components/ui/card";
 
 export default function DashboardPage() {
 	return (
 		<div className="min-h-screen bg-background p-8">
 			<div className="mx-auto max-w-7xl space-y-8">
-			<div className="space-y-4">
+				<div className="space-y-4">
 					<h2 id="workouts" className="text-2xl font-bold tracking-tight">
 						Workouts
 					</h2>
@@ -31,24 +39,41 @@ export default function DashboardPage() {
 							<h2 id="log-score" className="text-2xl font-bold tracking-tight">
 								Log Score
 							</h2>
-							<Button
-								asChild
-								className="bg-[#dbff43] text-black hover:bg-[#dbff43]/90 font-semibold"
-							>
-								<a
-									href="https://games.crossfit.com/workouts/open/2025"
-									target="_blank"
-									rel="noopener noreferrer"
-									className="flex items-center gap-2"
-								>
-									Log Score on CrossFit Games
-									<ArrowUpRight className="h-4 w-4" />
-								</a>
-							</Button>
 						</div>
 					</div>
 					<Suspense fallback={<LogWorkoutFormSkeleton />}>
-						<LogWorkoutForm />
+						<div className="flex flex-col sm:flex-row gap-4 w-full">
+							<LogWorkoutForm />
+							<Card className="max-w-2xl flex flex-col justify-between">
+								<CardHeader>
+									<CardTitle>Log Official CrossFit Open Score</CardTitle>
+									<CardDescription className="max-w-[300px]">
+										You will need to follow the link below to log your official
+										open score on the CrossFit Games leaderboard. Clicking the
+										link will take you to the submision page. 
+										<p className="text-sm text-muted-foreground mt-2">Scores are due by
+									Monday!</p>
+									</CardDescription>
+								</CardHeader>
+								<CardContent className="flex justify-center">
+									<Button
+											asChild
+											className="bg-[#dbff43] text-black hover:bg-[#dbff43]/90 font-semibold w-fit mx-auto"
+										>
+											<a
+												href="https://games.crossfit.com/workouts/open/2025"
+												target="_blank"
+												rel="noopener noreferrer"
+												className="flex items-center justify-center gap-2"
+											>
+												Log Score on CrossFit Games
+												<ArrowUpRight className="h-4 w-4" />
+											</a>
+										</Button>
+										
+								</CardContent>
+							</Card>
+						</div>
 					</Suspense>
 				</div>
 
@@ -58,8 +83,6 @@ export default function DashboardPage() {
 						<DashboardCards />
 					</Suspense>
 				</div>
-
-				
 
 				<div className="space-y-4">
 					<Suspense fallback={<AssignPointsFormSkeleton />}>
