3784391e97c74fbada035c7c881c15382985a726
Zac Jones <zacjones93@gmail.com>
2025-02-26 13:49:36 -0700

    fix: add athlete queries

--- src/lib/supabase/queries/server/athletes.ts
+++ src/lib/supabase/queries/server/athletes.ts
@@ -1,14 +1,13 @@
+import { User } from "@supabase/supabase-js";
 import { createServerClient } from "../../server";
 import { Tables } from "@/types/database.types";
 
 export type Athlete = Tables<"athletes">;
+export type Team = Tables<"teams">;
 
 export type AthleteWithTeams = Athlete & {
   athlete_teams: {
-    team: {
-      id: string;
-      name: string;
-    } | null;
+    team: Team | null;
   }[];
 };
 
@@ -20,10 +19,7 @@ export const getAthletesWithTeams = async () => {
       `
         *,
         athlete_teams!inner (
-          team:teams(
-            id,
-            name
-          )
+          team:teams!inner(*)
         )
       `
     )
@@ -31,4 +27,64 @@ export const getAthletesWithTeams = async () => {
     .throwOnError();
 
   return data;
+}
+
+export const getCurrentAthleteTeammates = async () => {
+  const supabase = await createServerClient();
+  const { data } = await supabase.auth.getUser();
+
+  if (!data?.user) {
+    throw new Error("User not found");
+  }
+
+  const currentAthlete = await getCurrentAthleteWithTeam();
+
+  const teamId = currentAthlete.athlete_teams[0]?.team?.id;
+
+  if (!teamId) {
+    throw new Error("Team not found");
+  }
+
+  const { data: teammates } = await supabase
+    .from("athletes")
+    .select(
+      `
+      *,
+      athlete_teams!inner (
+        team:teams!inner(*)
+      )
+    `
+    )
+    .eq("athlete_teams.team_id", teamId)
+    .order("name")
+    .throwOnError();
+
+
+  return teammates;
+}
+
+
+export const getCurrentAthleteWithTeam = async () => {
+  const supabase = await createServerClient();
+  const { data } = await supabase.auth.getUser();
+
+  if (!data?.user) {
+    throw new Error("User not found");
+  }
+
+  const { data: currentAthlete } = await supabase
+    .from("athletes")
+    .select(
+      `
+      *,
+      athlete_teams!inner (
+        team:teams(*)
+      )
+    `
+    )
+    .eq("user_id", data.user.id)
+    .single()
+    .throwOnError();
+
+  return currentAthlete;
 }
